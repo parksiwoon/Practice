4f78d6ffff880931b97f40ded8d3e6ef3c2258c5
parksiwoon <59051526+parksiwoon@users.noreply.github.com>
2024-08-09 13:58:36 +0900

    Revert "final_pro"

--- src/randomNumbers.js
+++
@@ -1,7 +0,0 @@
-export const generateRandomNumbers = () => {
-    const numbers = new Set();
-    while (numbers.size < 3) {
-        numbers.add(MissionUtils.Random.pickNumberInRange(1, 9));
-    }
-    return Array.from(numbers).join('');
-};
